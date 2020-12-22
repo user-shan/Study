50f0c04d5bdb45a696979e68f63cb37fae8731c9
user-shan <m15046264025@163.com>
2020-12-22 22:37:24 +0800

    sxy 更新小程序源代码

--- pages/logon/logon.js
+++ pages/logon/logon.js
@@ -1,41 +1,101 @@
 // pages/logon/logon.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
-
-  /**
-   * 组件的初始数据
-   */
+var app = getApp();
+Page({
   data: {
-
+    //用户个人信息
+    userInfo: {
+      avatarUrl: "", //用户头像
+      nickName: "", //用户昵称
+    },
+    //判断小程序的API，回调，参数，组件等是否在当前版本可用。
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    // 通过变量判断用户是否登录过
+    isHide: false
   },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
+  onShow: function () {
+    const userInfo = wx.getStorageSync("userInfo");
+    this.setData({
+      userInfo
+    })
   },
-  doLogin: function (e) {
-    console.log(e)
-    wx.login({
+  handleGetUserInfo(e) {
+    const {
+      userInfo
+    } = e.detail;
+    wx.setStorageSync("userInfo", userInfo);
+    wx.navigateBack({
+      delta: 1
+    });
+  },
+  onLoad: function () {
+    var that = this;
+    // 查看是否授权
+    wx.getSetting({
       success: function (res) {
-        console.log(res)
-        //获取登录临时凭证
-        var code = res.code;
-        //调用后端接口 获取微信的session_key 和 openID
-        wx.request({
-          url: 'http://localhost:8080/wxLogin?code=' + code,
-          method: "post",
-          success: function (resule) {
-            console.log(resule);
-          }
+        if (res.authSetting['scope.userInfo']) {
+          wx.getUserInfo({
+            success: function (res) {
+              // 实现在用户授权成功后，调用微信的 wx.login 接口，从而获取code
+              wx.login({
+                success: res => {
+                  console.log("用户的code:" + res.code);
+                }
+              });
+            }
+          });
+        } else {
+          // 用户没有授权，显示授权页面
+          that.setData({
+            isHide: true
+          });
+        }
+      }
+    });
+    var that = this;
+    /**
+     * 获取用户信息
+     */
+    wx.getUserInfo({
+      success: function (res) {
+        console.log(res);
+        var avatarUrl = 'userInfo.avatarUrl';
+        var nickName = 'userInfo.nickName';
+        that.setData({
+          [avatarUrl]: res.userInfo.avatarUrl,
+          [nickName]: res.userInfo.nickName,
         })
       }
     })
+  },
+
+  /**
+   * 授权方法
+   */
+  bindGetUserInfo: function (e) {
+    if (e.detail.userInfo) {
+      //用户按了允许授权按钮
+      var that = this;
+      // 获取到用户的信息了，打印到控制台上看下
+      console.log("用户的信息=", e.detail.userInfo);
+      //授权成功后,通过改变 isHide 的值，让实现页面显示出来，把授权页面隐藏起来
+      that.setData({
+        isHide: false
+      });
+    } else {
+      //用户按了拒绝按钮
+      wx.showModal({
+        title: '您好',
+        content: '您点击了拒绝授权',
+        showCancel: false,
+        confirmText: '是否继续',
+        // 点击成功方法
+        success: function (res) {
+          // 用户没有授权成功，不需要改变 isHide 的值
+          if (res.confirm) {
+            console.log('用户选择继续');
+          }
+        }
+      });
+    }
   }
 })
